a296d86f1f82aefa24453efae31a14bf25d479d0
ffolabear <ffolabear@gmail.com>
2022-11-12 17:27:04 +0900

    feat(InputValidation): 입력값 검증을 위한 인터페이스 생성

---
+++ src/main/java/lotto/validation/InputValidation.java
@@ -0,0 +1,10 @@
+package lotto.validation;
+
+import java.util.List;
+
+public interface InputValidation {
+
+    public void isInputInteger(String input);
+
+    public int convertToInteger(String input);
+}
