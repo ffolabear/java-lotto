cc5782d2ebc62f5956e68757653336cd848850c4
ffolabear <ffolabear@gmail.com>
2022-11-14 03:05:23 +0900

    feat(RankDetail): 등수에 관한 정보들을 좀더 객체지향적으로 관리하기 위해 수정

--- src/main/java/lotto/view/RankDetail.java
+++ src/main/java/lotto/view/RankDetail.java
@@ -2,21 +2,32 @@ package lotto.view;
 
 public enum RankDetail {
 
-    FIRST("6개 일치 (2,000,000,000원) - "),
-    SECOND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    SECOND("5개 일치 (1,500,000원) - "),
-    THIRD("4개 일치 (50,000원) - "),
-    FOURTH("3개 일치 (5,000원) - "),
-    UNIT("개\n")
+    FIRST(6, 2000000000, 1),
+    SECOND_BONUS(5, 30000000, 2),
+    SECOND(5, 1500000, 3),
+    THIRD(4, 50000, 4),
+    FOURTH(3, 5000, 5),
     ;
 
-    private final String rankMessage;
+    private final int matchAmount;
+    private final int prize;
+    private final int rank;
 
-    RankDetail(String rankMessage) {
-        this.rankMessage = rankMessage;
+    RankDetail(int matchAmount, int prize, int rank) {
+        this.matchAmount = matchAmount;
+        this.prize = prize;
+        this.rank = rank;
     }
 
-    public String getRankMessage() {
-        return rankMessage;
+    public int getMatchAmount() {
+        return matchAmount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public int getRank() {
+        return rank;
     }
 }
