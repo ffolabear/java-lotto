84ea792c907bdb8b3d86a653d2610bcd327c5980
ffolabear <ffolabear@gmail.com>
2022-11-14 03:23:37 +0900

    feat(LottoService): 구매 금액 입력 ~ 보너스 번호 입력까지 진행하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,26 +1,72 @@
 package lotto.service;
 
 import lotto.util.Input;
+import lotto.util.ResultMessage;
+import lotto.util.SystemMessage;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoService {
 
     Input input;
-    List<List<Integer>> purchasedLotto;
+    SystemMessage message = new SystemMessage();
+    List<List<Integer>> generatedLotto;
+    List<Integer> winningNumbers;
+    int bonusNumber;
+    int ticket;
 
     public LottoService() {
         input = new Input();
-        purchaseLotto();
+        startService();
     }
 
+    public void startService() {
+        message.inputMoneyMessage();
+        purchaseLotto();
+        generateLotto(ticket);
+        inputWinningNumbers();
+        inputBonusNumber();
+        startDraw();
+    }
 
-    public void purchaseLotto() {
+    private void purchaseLotto() {
         input.inputUserMoney();
-        int ticket = input.getLottoTicket();
-        PurchaseLotto purchaseLotto = new PurchaseLotto(ticket);
-        purchasedLotto = purchaseLotto.getPurchasedLottoList();
-        System.out.println(purchasedLotto);
+        ticket = input.getLottoTicket();
+        message.purchaseResultMessage(ticket);
+    }
+
+    private void generateLotto(int ticket) {
+        generatedLotto = new ArrayList<>();
+        GenerateLotto generateLotto = new GenerateLotto(ticket);
+        generatedLotto = generateLotto.getPurchasedLottoList();
+        message.printGeneratedLotto(generatedLotto);
+    }
+
+    private void inputWinningNumbers() {
+        winningNumbers = new ArrayList<>();
+        message.inputWinningNumbersMessage();
+        input.inputWinningNumbers();
+        winningNumbers = input.getWinningNumbers();
+    }
+
+    private void inputBonusNumber() {
+        bonusNumber = 0;
+        message.inputBonusNumberMessage();
+        input.inputBonusNumber(winningNumbers);
+        bonusNumber = input.getBonusNumber();
+    }
+
+    private void startDraw() {
+        LottoDraw lottoDraw = new LottoDraw(generatedLotto, winningNumbers, bonusNumber);
+        lottoDraw.startDraw();
+        Map<Integer, Integer> lottoResult = lottoDraw.getDrawResult();
+        int bonusMatched = lottoDraw.getBonusNumberMatched();
+//        ResultMessage resultMessage = new ResultMessage(lottoResult, bonusMatched);
+        List<Integer> keyset = lottoResult.keySet().stream().sorted().collect(Collectors.toList());
+        System.out.println("LottoService.startDraw : " + keyset);
     }
 
 }
