8a6ee9d864da25458bd1b5ebbc1c49111bb64194
ffolabear <ffolabear@gmail.com>
2022-11-15 08:13:15 +0900

    fix(Application): 에러를 발생시키고 오류 메세지를 출력 시키기 위해 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,10 @@ import lotto.service.LottoService;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoService lottoService = new LottoService();
+        try {
+            LottoService lottoService = new LottoService();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
