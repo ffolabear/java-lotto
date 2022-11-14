acc69876dd2e574b35719cf20cc95ccc9548d9db
ffolabear <ffolabear@gmail.com>
2022-11-15 08:05:01 +0900

    chore(RankDetail): enum 객체에 상수 추가

--- src/main/java/lotto/view/RankDetail.java
+++ src/main/java/lotto/view/RankDetail.java
@@ -2,17 +2,19 @@ package lotto.view;
 
 public enum RankDetail {
 
-    FIRST(6, 2000000000),
-    SECOND_BONUS(5, 30000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
+    FIRST("first", 6, 2000000000),
+    BONUS("bonus", 5, 30000000),
+    SECOND("second", 5, 1500000),
+    THIRD("third", 4, 50000),
+    FOURTH("fourth", 3, 5000),
     ;
 
+    private final String rank;
     private final int matchAmount;
     private final int prize;
 
-    RankDetail(int matchAmount, int prize) {
+    RankDetail(String rank, int matchAmount, int prize) {
+        this.rank = rank;
         this.matchAmount = matchAmount;
         this.prize = prize;
     }
@@ -25,4 +27,7 @@ public enum RankDetail {
         return prize;
     }
 
+    public String getRank() {
+        return rank;
+    }
 }
