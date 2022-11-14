b9dc353b711cfa379aae51af2224d606851d58f3
ffolabear <ffolabear@gmail.com>
2022-11-15 03:04:47 +0900

    chore(LottoSetting): 상수 선언 후 줄바꿈 추가

--- src/main/java/lotto/validation/LottoSetting.java
+++ src/main/java/lotto/validation/LottoSetting.java
@@ -2,7 +2,9 @@ package lotto.validation;
 
 public enum LottoSetting {
 
-    START_NUMBER(1), END_NUMBER(45), NUMBER_AMOUNT(6);
+    START_NUMBER(1),
+    END_NUMBER(45),
+    NUMBER_AMOUNT(6);
 
     private final int value;
 
