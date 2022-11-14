a87d96a7c110a0b3c60bf6e81bb6aadb4f26a6de
ffolabear <ffolabear@gmail.com>
2022-11-15 08:02:14 +0900

    chore(RankMessageDetail): 상수 이름 변경

--- src/main/java/lotto/view/RankMessageDetail.java
+++ src/main/java/lotto/view/RankMessageDetail.java
@@ -6,7 +6,7 @@ public enum RankMessageDetail {
     RESULT_TITLE_LINE("---\n"),
     BONUS_MATCHED(", 보너스 볼 일치"),
     MATCH_RESULT("%s개 일치%s (%s원) - %s개"),
-    PROFIT_RATIO("총 수익률은 %s%%입니다.")
+    YIELD("총 수익률은 %s%%입니다.")
     ;
 
     private final String message;
