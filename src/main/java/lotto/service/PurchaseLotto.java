4a638c5cee01b8d328948b8ce95242fa0d826167
ffolabear <ffolabear@gmail.com>
2022-11-15 03:16:29 +0900

    feat(PurchaseLotto): 로또 클래스를 사용하여 로또 생성하는 기능 추가

---
+++ src/main/java/lotto/service/PurchaseLotto.java
@@ -0,0 +1,38 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.validation.LottoSetting.*;
+
+public class PurchaseLotto {
+
+    int ticket;
+    List<List<Integer>> purchasedLotto;
+
+    public PurchaseLotto(int ticket) {
+        this.ticket = ticket;
+        generateLottoList();
+    }
+
+    public List<List<Integer>> getPurchasedLottoList() {
+        return purchasedLotto;
+    }
+
+    private void generateLottoList() {
+        purchasedLotto = new ArrayList<>();
+        while (ticket > 0) {
+            Lotto lotto = new Lotto(
+                    Randoms.pickUniqueNumbersInRange(
+                            START_NUMBER.getValue(),
+                            END_NUMBER.getValue(),
+                            NUMBER_AMOUNT.getValue()));
+            ticket--;
+            purchasedLotto.add(lotto.getNumbers());
+        }
+    }
+
+}
