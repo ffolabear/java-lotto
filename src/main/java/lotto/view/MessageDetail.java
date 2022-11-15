f931cb292193751bd416dd0120b25cd270a853b9
ffolabear <ffolabear@gmail.com>
2022-11-15 20:00:05 +0900

    chore(MessageDetail): 추첨 결과 메세지 생성 방식으로 변경

--- src/main/java/lotto/view/MessageDetail.java
+++ src/main/java/lotto/view/MessageDetail.java
@@ -3,7 +3,7 @@ package lotto.view;
 public enum MessageDetail {
 
     INPUT_MONEY("구입금액을 입력해 주세요."),
-    PURCHASE_RESULT("개를 구매했습니다."),
+    PURCHASE_RESULT("%s개를 구매했습니다."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     ;
