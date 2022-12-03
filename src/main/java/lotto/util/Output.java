c2cd38f5bd37e0bf2667a469b9c2926940753a66
ffolabear <ffolabear@gmail.com>
2022-12-03 23:54:36 +0900

    feat(Output): 출력클래스 생성

---
+++ src/main/java/lotto/util/Output.java
@@ -0,0 +1,26 @@
+package lotto.util;
+
+import lotto.service.Lotto;
+import lotto.view.LottoMessage;
+
+import java.util.List;
+
+public class Output {
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printLottoPurchaseMessage(long ticket) {
+        System.out.printf((LottoMessage.PURCHASE_MESSAGE.getMessage()) + "%n", ticket);
+    }
+
+    public void printLotto(List<Lotto> purchasedLotto) {
+        StringBuilder sb = new StringBuilder();
+        for (Lotto lotto : purchasedLotto) {
+            sb.append(lotto.toString()).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
