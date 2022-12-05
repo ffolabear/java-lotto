94185ab2733a75bc4ed45e7c1b755da08eb74146
ffolabear <ffolabear@gmail.com>
2022-12-05 21:54:43 +0900

    feat(MoneyValidation): 로또 구매금액 입력 검증 클래스 생성

--- src/main/java/lotto/validation/MoneyValidation.java
+++ src/main/java/lotto/validation/MoneyValidation.java
@@ -2,6 +2,9 @@ package lotto.validation;
 
 import lotto.view.LottoErrors;
 
+import static lotto.view.LottoErrors.ERROR_NON_DIGIT;
+import static lotto.view.LottoErrors.ERROR_NON_DIVIDABLE;
+
 public class MoneyValidation implements Predicate {
 
     @Override
@@ -11,22 +14,31 @@ public class MoneyValidation implements Predicate {
             isMoneyDividable(input);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            throw new IllegalArgumentException();
         }
+    }
+
+    @Override
+    public void printError(LottoErrors lottoErrors){
+        throw new IllegalArgumentException(lottoErrors.getError());
     }
 
     private void isMoneyDigit(String input) {
         if (!input.matches("[0-9]+")) {
-            throw new IllegalArgumentException(LottoErrors.ERROR_NON_DIGIT.getError());
+            printError(ERROR_NON_DIGIT);
+            throw new IllegalArgumentException();
         }
     }
 
     private void isMoneyDividable(String input) {
         if (convertToLong(input) % 1000 != 0) {
-            throw new IllegalArgumentException(LottoErrors.ERROR_NON_DIVIDABLE.getError());
+            printError(ERROR_NON_DIVIDABLE);
+            throw new IllegalArgumentException();
         }
     }
 
     private long convertToLong(String input) {
         return Long.parseLong(input);
     }
+
 }
