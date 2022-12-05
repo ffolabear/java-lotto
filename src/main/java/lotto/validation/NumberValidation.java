b5ca0ad3315bba3b854f71c4c7fd572cbe1cafeb
ffolabear <ffolabear@gmail.com>
2022-12-05 21:55:07 +0900

    feat(NumberValidation): 당첨 번호 입력 검증 클래스 생성

--- src/main/java/lotto/validation/NumberValidation.java
+++ src/main/java/lotto/validation/NumberValidation.java
@@ -3,6 +3,12 @@ package lotto.validation;
 import lotto.service.LottoSetting;
 import lotto.view.LottoErrors;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.view.LottoErrors.*;
+
+
 public class NumberValidation implements Predicate{
 
     private final String BREAK_POINT = ",";
@@ -11,21 +17,27 @@ public class NumberValidation implements Predicate{
     public void test(String input) {
         try {
             isValidNumberAmount(input);
-            numberRangeCheck(input);
+            numberRangeCheck(convertToList(input));
+            isDuplicateNumber(convertToList(input));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            throw new IllegalArgumentException();
         }
+    }
+
+    @Override
+    public void printError(LottoErrors lottoErrors) {
+        throw new IllegalArgumentException(lottoErrors.getError());
     }
 
     private void isValidNumberAmount(String input) {
         if (input.split(BREAK_POINT).length != LottoSetting.NUMBER_AMOUNT.getAttribute()) {
-            throw new IllegalArgumentException(LottoErrors.ERROR_NUMBER_AMOUNT.getError());
+            printError(ERROR_NUMBER_AMOUNT);
         }
     }
 
-    private void numberRangeCheck(String input) {
-        String[] numbers = input.split(BREAK_POINT);
-        for (String number : numbers) {
+    private void numberRangeCheck(String[] convertedInput) {
+        for (String number : convertedInput) {
             isMoneyDigit(number);
             isValidNumberRange(number);
         }
@@ -33,7 +45,7 @@ public class NumberValidation implements Predicate{
 
     private void isMoneyDigit(String input) {
         if (!input.matches("[0-9]+")) {
-            throw new IllegalArgumentException(LottoErrors.ERROR_NON_DIGIT.getError());
+            printError(ERROR_NON_DIGIT);
         }
     }
 
@@ -41,8 +53,20 @@ public class NumberValidation implements Predicate{
         int convertedNumber = Integer.parseInt(number);
         if (convertedNumber < LottoSetting.START_NUMBER.getAttribute()
                 && LottoSetting.END_NUMBER.getAttribute() < convertedNumber) {
-            throw new IllegalArgumentException(LottoErrors.ERROR_NUMBER_RANGE.getError());
+//            throw new IllegalArgumentException(ERROR_NUMBER_RANGE.getError());
+            printError(ERROR_NUMBER_RANGE);
         }
+    }
+
+    private void isDuplicateNumber(String[] convertedInput) {
+        List<String> duplicateCheck = List.of(convertedInput).stream().distinct().collect(Collectors.toList());
+        if (convertedInput.length != duplicateCheck.size()) {
+            printError(ERROR_DUPLICATE_NUMBER);
+        }
+    }
+
+    private String[] convertToList(String input) {
+        return input.split(BREAK_POINT);
     }
 
 }
