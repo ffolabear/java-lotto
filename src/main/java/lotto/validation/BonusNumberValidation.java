eff0dba737dfe50efe5b31a2eeb21328f7f91279
ffolabear <ffolabear@gmail.com>
2022-12-05 21:55:55 +0900

    feat(BonusNumberValidation): 보너스 번호 입력 검증 클래스 생성

---
+++ src/main/java/lotto/validation/BonusNumberValidation.java
@@ -0,0 +1,38 @@
+package lotto.validation;
+
+import lotto.view.LottoErrors;
+
+import java.util.List;
+
+import static lotto.view.LottoErrors.ERROR_INVALID_BONUS_NUMBER;
+
+public class BonusNumberValidation implements Predicate {
+
+    private final List<Integer> winningNumbers;
+
+    public BonusNumberValidation(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    @Override
+    public void test(String input) {
+        try {
+            isDuplicateNumber(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public void printError(LottoErrors lottoErrors) {
+        throw new IllegalArgumentException(lottoErrors.getError());
+    }
+
+    private void isDuplicateNumber(String input) {
+        if (winningNumbers.contains(Integer.parseInt(input))) {
+            printError(ERROR_INVALID_BONUS_NUMBER);
+        }
+    }
+
+}
