8d906fcb463f3a347ded36f83983da0f1e9019dc
ffolabear <ffolabear@gmail.com>
2022-12-05 21:57:12 +0900

    feat(Input): 사용자의 입력을 담당하는 클래스 생성

--- src/main/java/lotto/util/Input.java
+++ src/main/java/lotto/util/Input.java
@@ -2,6 +2,7 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.service.LottoSetting;
+import lotto.validation.BonusNumberValidation;
 import lotto.validation.MoneyValidation;
 import lotto.validation.NumberValidation;
 
@@ -10,34 +11,43 @@ import java.util.List;
 
 public class Input {
 
-    public long InputMoney() {
+    public long inputMoney() {
         String money = Console.readLine();
         MoneyValidation moneyValidation = new MoneyValidation();
         moneyValidation.test(money);
         return convertToTicket(money);
     }
 
-    public List<Integer> InputWinningNumber() {
+    public List<Integer> inputWinningNumber() {
         String numbers = Console.readLine();
         NumberValidation numberValidation = new NumberValidation();
         numberValidation.test(numbers);
         return convertToIntegerList(numbers);
     }
 
-    public long convertToLong(String money) {
+    public int inputBonusNumber(List<Integer> winningNumbers) {
+        String number = Console.readLine();
+        NumberValidation numberValidation = new NumberValidation();
+        numberValidation.test(number);
+        BonusNumberValidation bonusNumberValidation = new BonusNumberValidation(winningNumbers);
+        bonusNumberValidation.test(number);
+        return convertToInteger(number);
+    }
+
+    private long convertToLong(String money) {
         return Long.parseLong(money);
     }
 
-    public long convertToTicket(String money) {
+    private long convertToTicket(String money) {
         long convertedMoney = convertToLong(money);
         return convertedMoney / LottoSetting.MONEY_UNIT.getAttribute();
     }
 
-    public int convertToInteger(String input) {
+    private int convertToInteger(String input) {
         return Integer.parseInt(input);
     }
 
-    public List<Integer> convertToIntegerList(String numbers) {
+    private List<Integer> convertToIntegerList(String numbers) {
         String[] seperatedNumbers = numbers.split(",");
         List<Integer> convertedNumbers = new ArrayList<>();
         for (String number : seperatedNumbers) {
