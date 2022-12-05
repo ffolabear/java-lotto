c9dd88881c84e3d92d8d60f42c127fdc7ad0c484
ffolabear <ffolabear@gmail.com>
2022-12-05 22:07:49 +0900

    feat(Application): 어플리케이션 실행 부분 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.service.LottoController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController controller = new LottoController();
+        controller.startLotto();
+            controller.startLotto();
     }
 }
