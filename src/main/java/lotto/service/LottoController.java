d800597bb4db7d1c9d75bcb5e52aed6f96a6e924
ffolabear <ffolabear@gmail.com>
2022-12-05 22:00:25 +0900

    feat(LottoController): 로또 컨트롤러 생성

---
+++ src/main/java/lotto/service/LottoController.java
@@ -0,0 +1,48 @@
+package lotto.service;
+
+import lotto.util.Input;
+import lotto.util.Output;
+import lotto.view.LottoMessage;
+
+import java.util.List;
+
+public class LottoController {
+
+    private final Output output = new Output();
+    private final Input input = new Input();
+    private List<Lotto> purchasedLotto;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public void startLotto() {
+        output.printMessage(LottoMessage.START_MESSAGE.getMessage());
+        purchaseLotto(input.inputMoney());
+        setWinningNumber();
+        drawLotto(purchasedLotto, winningNumbers);
+        setBonusNumber();
+    }
+
+    public void purchaseLotto(long ticket) {
+        output.printLottoPurchaseMessage(ticket);
+        PurchaseLotto purchaseLotto = new PurchaseLotto(ticket);
+        purchaseLotto.purchase();
+        purchasedLotto = purchaseLotto.getPurchasedLotto();
+        output.printLotto(purchasedLotto);
+    }
+
+    public void setWinningNumber() {
+        output.printMessage(LottoMessage.WINNING_NUMBER_MESSAGE.getMessage());
+        winningNumbers = input.inputWinningNumber();
+    }
+
+    public void setBonusNumber() {
+        output.printMessage(LottoMessage.BONUS_NUMBER_MESSAGE.getMessage());
+        bonusNumber = input.inputBonusNumber(winningNumbers);
+    }
+
+    public void drawLotto(List<Lotto> purchasedLotto, List<Integer> winningNumbers) {
+
+    }
+
+
+}
