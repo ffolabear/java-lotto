eeb45d13021087bb7a08733f1810d0f6c424f5ce
ffolabear <ffolabear@gmail.com>
2022-12-08 15:34:35 +0900

    feat(LottoErrors): 새로운 에러 추가

--- src/main/java/lotto/view/LottoErrors.java
+++ src/main/java/lotto/view/LottoErrors.java
@@ -5,6 +5,8 @@ import lotto.service.LottoSetting;
 public enum LottoErrors {
 
     ERROR_NON_DIGIT("입력값은 반드시 숫자여야 합니다."),
+    ERROR_NUMBER_GENERATE_FAIL("자동 번호 생성에 예측하지 못한 오류가 발생하였습니다."),
+    ERROR_MONEY_EXCESS("구매할 수 있는 금액을 초과 하였습니다."),
     ERROR_NON_DIVIDABLE(String.format("금액은 %s원 단위로 입력해야 합니다.", LottoSetting.MONEY_UNIT.getAttribute())),
     ERROR_NUMBER_AMOUNT(String.format("당첨 번호는 반드시 %s개의 번호를 쉼표로 구분해서 입력헤야 합니다.",
             LottoSetting.NUMBER_AMOUNT.getAttribute())),
