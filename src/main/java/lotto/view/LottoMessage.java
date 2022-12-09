e27458ea922367541fe51b01f90ed127898d0978
ffolabear <ffolabear@gmail.com>
2022-12-09 22:51:12 +0900

    feat(LottoMessage): 추첨 결과 메세지 제목 추가

--- src/main/java/lotto/view/LottoMessage.java
+++ src/main/java/lotto/view/LottoMessage.java
@@ -5,7 +5,9 @@ public enum LottoMessage {
     START_MESSAGE("구입금액을 입력해 주세요."),
     PURCHASE_MESSAGE("\n%s개를 구매하였습니다."),
     WINNING_NUMBER_MESSAGE("\n당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_MESSAGE("\n보너스 번호를 입력해 주세요.");
+    BONUS_NUMBER_MESSAGE("\n보너스 번호를 입력해 주세요."),
+    RESULT_MESSAGE_HEADER("당첨 통계\n"),
+    RESULT_MESSAGE_BORDER("---\n");
 
     private final String message;
 
