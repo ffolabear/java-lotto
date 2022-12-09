84c6adacfe91dbf24b8ba9ea28b2587420d6b096
ffolabear <ffolabear@gmail.com>
2022-12-09 22:49:51 +0900

    feat(DrawResultGenerator): 추첨 결과 메세지 생성 기능 구현

--- src/main/java/lotto/view/DrawResultGenerator.java
+++ src/main/java/lotto/view/DrawResultGenerator.java
@@ -7,18 +7,45 @@ import java.util.*;
 public class DrawResultGenerator {
 
     private final Map<Rank, Integer> rankMap;
+    private List<Rank> keySet;
 
     public DrawResultGenerator(Map<Rank, Integer> rankMap) {
         this.rankMap = rankMap;
-        System.out.println(rankMap);
         sortMap();
     }
 
     private void sortMap() {
-        List<Rank> keySet = new ArrayList<>(rankMap.keySet());
+        keySet = new ArrayList<>(rankMap.keySet());
         keySet.sort((o1, o2) -> o1.getRank() - o2.getRank());
-        System.out.println(keySet);
+
     }
 
+    public void generateResultMessage() {
+        System.out.println(LottoMessage.RESULT_MESSAGE_HEADER.getMessage());
+        System.out.println(LottoMessage.RESULT_MESSAGE_BORDER.getMessage());
+        for (Rank rank : keySet) {
+            printResultMessage(rank);
+        }
+    }
+
+    public void printResultMessage(Rank rank) {
+        System.out.println(checkRank(rank));
+
+    }
+
+    private String checkRank(Rank rank) {
+        if (rank == Rank.SECOND) {
+            return bonusResultMessage(rank);
+        }
+        return normalResultMessage(rank);
+    }
+
+    private String  normalResultMessage(Rank rank) {
+        return DrawResultMessage.WITHOUT_BONUS.getMessage(rank, rankMap.get(rank));
+    }
+
+    private String bonusResultMessage(Rank rank) {
+        return DrawResultMessage.WITH_BONUS.getMessage(rank, rankMap.get(rank));
+    }
 
 }
