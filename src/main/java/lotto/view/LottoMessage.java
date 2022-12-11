c8923755ad15ac2bdefa1ffd8c85f90124b67ab1
ffolabear <ffolabear@gmail.com>
2022-12-12 02:02:48 +0900

    chore(LottoMessage): 메세지 상수 수정

--- src/main/java/lotto/view/LottoMessage.java
+++ src/main/java/lotto/view/LottoMessage.java
@@ -6,8 +6,8 @@ public enum LottoMessage {
     PURCHASE_MESSAGE("\n%s개를 구매하였습니다."),
     WINNING_NUMBER_MESSAGE("\n당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_MESSAGE("\n보너스 번호를 입력해 주세요."),
-    RESULT_MESSAGE_HEADER("당첨 통계\n"),
-    RESULT_MESSAGE_BORDER("---\n");
+    RESULT_MESSAGE_HEADER("당첨 통계"),
+    RESULT_MESSAGE_BORDER("---");
 
     private final String message;
 
