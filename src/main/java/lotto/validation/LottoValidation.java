8ca44c5435abdcaa126b2ec2d241c8fc604e91ae
ffolabear <ffolabear@gmail.com>
2022-12-12 13:43:33 +0900

    fix(LottoValidation): 유효하지 않은 로또를 검증할 수 있게 수정

--- src/main/java/lotto/validation/LottoValidation.java
+++ src/main/java/lotto/validation/LottoValidation.java
@@ -8,28 +8,44 @@ public class LottoValidation {
 
     private final List<Integer> lottoNumbers;
     private boolean isValidLotto = true;
-    private final CommonValidation commonValidation = new CommonValidation();
 
     public LottoValidation(List<Integer> lottoNumbers) {
         this.lottoNumbers = lottoNumbers;
     }
+
     public void validate() {
         isValidAmount();
         numberRangeCheck();
+        isDuplicateNumber();
         if (!isValidLotto) {
-            throw new RuntimeException();
+            throw new IllegalArgumentException();
         }
     }
 
     private void isValidAmount() {
         if (lottoNumbers.size() != LottoSetting.NUMBER_AMOUNT.getAttribute()) {
             isValidLotto = false;
+            throw new IllegalArgumentException();
         }
     }
 
     private void numberRangeCheck() {
         for (int number : lottoNumbers) {
-            commonValidation.isValidNumberRange(number);
+            isValidNumberRange(number);
+        }
+    }
+
+    private void isValidNumberRange(int number) {
+        if (number < LottoSetting.START_NUMBER.getAttribute()
+                && LottoSetting.END_NUMBER.getAttribute() < number) {
+            isValidLotto = false;
+        }
+    }
+
+    private void isDuplicateNumber() {
+        int numberCount = (int)lottoNumbers.stream().distinct().count();
+        if (numberCount != lottoNumbers.size()) {
+            throw new IllegalArgumentException();
         }
     }
 
