cb076b8eeb9ca759769f00a119ec4ace4ebd8b2a
ffolabear <ffolabear@gmail.com>
2022-12-12 13:45:01 +0900

    chore(Application): 어플리케이션 실행 부분 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,11 @@ import lotto.service.LottoController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoController controller = new LottoController();
-        controller.startLotto();
+        try {
+            LottoController controller = new LottoController();
+            controller.startLotto();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 }
