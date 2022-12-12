62c0fdd407fd0289b6a855b606f11b98278f3e18
ffolabear <ffolabear@gmail.com>
2022-12-12 13:45:56 +0900

    fix(CommonValidation): 검증 로직 수정중

--- src/main/java/lotto/validation/CommonValidation.java
+++ src/main/java/lotto/validation/CommonValidation.java
@@ -10,30 +10,39 @@ public class CommonValidation implements Predicate{
 
     @Override
     public void test(String input) {
-        try {
-            isInputDigit(input);
-            isValidNumberRange(convertToInteger(input));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            throw new IllegalArgumentException();
-        }
+//        try {
+//            isInputDigit(input);
+//            isValidNumberRange(convertToInteger(input));
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+////            throw new IllegalArgumentException(e.getMessage());
+//        }
+
+        isInputDigit(input);
+        isValidNumberRange(convertToInteger(input));
     }
 
     public void isInputDigit(String input) {
         if (!input.matches("[0-9]+")) {
-            printError(ERROR_NON_DIGIT);
+//            printError(ERROR_NON_DIGIT);
+            throw new IllegalArgumentException(errorMessage(ERROR_NON_DIGIT));
         }
     }
 
     public void isValidNumberRange(int number) {
         if (number < LottoSetting.START_NUMBER.getAttribute()
                 && LottoSetting.END_NUMBER.getAttribute() < number) {
-            printError(ERROR_NUMBER_RANGE);
+//            printError(ERROR_NUMBER_RANGE);
+            throw new IllegalArgumentException(errorMessage(ERROR_NUMBER_RANGE));
         }
     }
 
+    public String errorMessage(LottoErrors errors) {
+        return errors.getError();
+    }
+
     @Override
-    public void printError(LottoErrors lottoErrors) {
+    public void printError(LottoErrors lottoErrors) throws IllegalArgumentException{
         throw new IllegalArgumentException(lottoErrors.getError());
     }
 
