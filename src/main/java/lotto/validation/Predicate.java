bf46b5164a15e6c2b47390a0cda91dee3651c6dd
ffolabear <ffolabear@gmail.com>
2022-12-12 13:53:41 +0900

    fix(Predicate): 검증 인터페이스 수정

--- src/main/java/lotto/validation/Predicate.java
+++ src/main/java/lotto/validation/Predicate.java
@@ -6,7 +6,7 @@ public interface Predicate {
 
     void test(String input);
 
-    void printError(LottoErrors lottoErrors);
+    String printError(LottoErrors lottoErrors);
 
     int convertToInteger(String input);
 
