326cf418e9fe40348ec208b43b3ac11bca65379f
ffolabear <ffolabear@gmail.com>
2022-12-12 13:57:16 +0900

    test(InputTest): 테스트 코드 수정

--- src/test/java/lotto/util/InputTest.java
+++ src/test/java/lotto/util/InputTest.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.Application;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,24 +14,33 @@ public class InputTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    @Nested
     @DisplayName("구매 금액 테스트")
-    @ParameterizedTest
-    @ValueSource(strings = {"apple", "222", "  "})
-    void moneyInputTest(String input) {
-        assertSimpleTest(() -> {
-            run(input);
-            Assertions.assertThat(output()).contains(ERROR_MESSAGE);
-        });
+    class MoneyInputTest {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"apple", "222", "  "})
+        void moneyInputTest(String input) {
+            assertSimpleTest(() -> {
+                run(input);
+                Assertions.assertThat(output()).contains(ERROR_MESSAGE);
+            });
+        }
     }
 
+    @Nested
     @DisplayName("당첨번호 입력 테스트")
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,4,5,46", "1,2,3,4,5", "1,2,3,4,47", "1,1,1,2,3,4","  "})
-    void numberInputTest(String input) {
-        assertSimpleTest(() -> {
-            run(input);
-            Assertions.assertThat(output()).contains(ERROR_MESSAGE);
-        });
+    class BonusInputTest {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1,2,3,4,5,46", "1,2,3,4,5", "1,2,3,4,47", "1,1,1,2,3,4", "  "})
+        void numberInputTest(String input) {
+            assertSimpleTest(() -> {
+                run(input);
+                Assertions.assertThat(output()).contains(ERROR_MESSAGE);
+            });
+        }
+
     }
 
     @Override
